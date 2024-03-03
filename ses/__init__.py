d1ab79abe2b51fc0ba31a66d1a1c512dcb310b8b
iaBnBashar <160961173+iaBnBashar@users.noreply.github.com>
2024-03-03 13:16:54 +0300

    Update __init__.py

--- ses/__init__.py
+++ ses/__init__.py
@@ -5,7 +5,6 @@ from config import Config
 from pyrogram import Client
 from rich.console import Console
 from rich.table import Table
-from HackSessionBot.Helpers.data import LOG_TEXT,ART
 from pyromod import listen 
 
 #getting variables
@@ -26,27 +25,27 @@ LOG = Console()
 logging.basicConfig(level=logging.INFO)
 
 #client
-app = Client(
-    "SupremeStark",
+bot = Client(
+    "bott",
     api_id = API_ID,
     api_hash = API_HASH,
     bot_token = TOKEN )
     
 
 
-async def HackSessionBot():
+async def sess():
     os.system("clear")
     header = Table(show_header=True, header_style="bold green")
     header.add_column(LOG_TEXT)
     LOG.print(header)
     LOG.print(f"[bold cyan]{ART}")
-    LOG.print("[bold yellow]تم عزيزي المواطن")
+    LOG.print("[bold yellow]")
     await app.start()    
     
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(HackSessionBot())    
+loop.run_until_complete(sess())    
 
 
 
